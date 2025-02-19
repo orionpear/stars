6aaea05e948249416a1006bdc525a6749f6276f2
Orionddr3 <96986933+Orionddr3@users.noreply.github.com>
2025-02-19 20:50:41 +0530

    Create an instance of the class Star to display a star
    So far there has been no star displayed on screen.
    
    Make a Star object and update the main loop to display the star.

--- stars.py
+++ stars.py
@@ -2,6 +2,7 @@ import sys
 import pygame
 
 from settings import Settings
+from star import Star
 
 def run_game():
     """Initialize the game objects and run the game."""
@@ -9,11 +10,14 @@ def run_game():
     screen = pygame.display.set_mode((settings.screen_width, settings.screen_height))
     pygame.display.set_caption("Stars")
 
+    star = Star(settings, screen)
+
     while True:
         for event in pygame.event.get():
             if event.type == pygame.QUIT:
                 sys.exit()
-        screen.fill((230, 230, 230))
+        screen.fill(settings.screen_bg_color)
+        star.blitme()
         pygame.display.flip()
 
 run_game()
